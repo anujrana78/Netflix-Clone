778f2f29ec652aacbda00b6aa5a66585eebcaf75
Anuj <anizrana@gmail.com>
2023-02-28 17:54:04 +0545

    Update ui with Hero from API

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,27 +1,51 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import requests from '../requests'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import requests from "../requests";
 
 const Main = () => {
-  
-    const [movies,setMovies] = useState([]) 
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async() => {
-            const response = await axios.get(requests.requestPopular)
-            const data = await response.data.results
-            setMovies(data)
-            console.log(data)
-        }
-        fetchData()
-    },[])
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(requests.requestPopular);
+      const data = await response.data.results;
+      setMovies(data);
+    };
+    fetchData();
+  }, []);
 
+  const randomMovie = movies[Math.floor(Math.random() * movies.length)];
+  console.log("Movie", randomMovie);
 
-    return (
-    <div>
-        <h2 className='text-white '>Main Page</h2>
+  // to create read more like on paragraph
+  const truncareString = (str,num) => {
+    if(str?.length > num) {
+        return str.slice(0,num) + "..."
+    }else return str
+  }
+  return (
+    <div className="w-full h-[550px]  text-white">
+      <div className="w-full h-full">
+        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+        <img
+          className="h-[550px] w-full object-cover"
+          src={`https://image.tmdb.org/t/p/original/${randomMovie?.backdrop_path}`}
+          alt="Heading"
+        />
+      </div>
+      <div className="ml-11 absolute top-[20%] lg:top-[30%]">
+        <h1 className="my-3 text-2xl md:text-5xl font-bold">{randomMovie?.title}</h1>
+        <div className="flex gap-2">
+          <button className="  text-black border bg-gray-300 p-2 px-5">
+            Play
+          </button>
+          <button className=" text-white border p-2 px-5">Watch Later</button>
+        </div>
+        <p className="text-sm text-gray-400 mt-4">Released Date : {randomMovie?.release_date}</p>
+        <p className="w-[40%] text-sm my-1">{truncareString(randomMovie?.overview,150)}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between p-4 z-(100) w-full absolute'>
-        <h1 className='text-red-600 text-5xl font-bold cursor-pointer'> NETFLIX</h1>
+    <div className='flex items-center justify-between p-6 z-(100) w-full absolute'>
+        <h1 className='text-red-600 lg:text-4xl text-4xl font-bold cursor-pointer z-10'> NETFLIX</h1>
         <div className='flex items-center justify-between p-4 gap-4'>
             <button className='text-white'>Sign in</button>
             <button className="bg-red-500 text-white font-bold py-2 px-4 rounded">Sign up</button>
